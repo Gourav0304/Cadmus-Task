4124211f6c5d3c9906116ac26cc1d663808b6623
Gourav0304 <gouravrathore0304@gmail.com>
2025-10-27 15:55:25 +0530

    chore(prisma): add prisma:build script to run migrations and generate client

--- server/src/app.ts
+++ server/src/app.ts
@@ -11,18 +11,18 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
-// ✅ Request logger middleware
+// middleware
 app.use((req, _res, next) => {
   logger.info(`${req.method} ${req.originalUrl}`);
   next();
 });
 app.use(errorHandler);
 
-// Health check route
+//  route
 app.use('/', healthRouter);
 app.use('/collab', collabRouter);
 
-// ❌ Error handler (always last)
+//  Error handler (always last)
 app.use((err: Error, _req: Request, res: Response, _next: NextFunction) => {
   logger.error(err.stack || err.message);
   res.status(500).json({ message: 'Something went wrong' });
